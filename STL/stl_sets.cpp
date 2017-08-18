01e38b026a58a0b6fd934e482e0037335f93dae2
muaazsalagar <salagarmi@gmail.com>
2017-08-18 16:23:14 -0400

    stl sets crud operations done

--- STL/stl_sets.cpp
+++ STL/stl_sets.cpp
@@ -9,24 +9,50 @@ using namespace std;
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    set<>
+    
     int times;
     cin>> times;
+    
+    
+    set <int> myset;
     int operation, input;
+  
     for (int i=0;i<times;i++){
        
-        for(int j =0;j<2;j++){
+       
             cin>>operation;
             cin>> input;
-            
+       
+         
             if(operation==1){
-                
+               
+                myset.insert(input);
+              
                 
             }
-            
-            
-        }
+            else if (operation==2)
+            {
+              
+                myset.erase(input); 
+              
+            }
+            else{
+               
+                
+                  set<int>::iterator myiterator=myset.find(input);
+                
+                if(myiterator!=myset.end()){
+                    cout <<"Yes\n";
+                    
+                }
+                else{
+                    cout<<"No\n";
+                }
+                
+            }
         
+    
+       
     }
     
     return 0;
