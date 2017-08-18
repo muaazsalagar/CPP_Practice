0843b904464a93f21ff9e77c623321b4b3fb025b
muaazsalagar <salagarmi@gmail.com>
2017-08-18 15:10:22 -0400

    stl sets crud operations

---
+++ STL/stl_sets.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    set<>
+    int times;
+    cin>> times;
+    int operation, input;
+    for (int i=0;i<times;i++){
+       
+        for(int j =0;j<2;j++){
+            cin>>operation;
+            cin>> input;
+            
+            if(operation==1){
+                
+                
+            }
+            
+            
+        }
+        
+    }
+    
+    return 0;
+}
